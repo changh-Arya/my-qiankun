24a8e48e9fc785b49d5c5b5974644273dafab883
xuchaobo <18628317778@sina.cn>
2023-03-20 16:08:01 +0800

    初始化提交

---
+++ reactdemo/src/pages/List.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function List() {
+  return (
+    <div>
+      <h2 class="testText">测试文本</h2>
+      商品列表
+    </div>
+  )
+}
---
+++ vuedemo/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h2 class="testText">测试文本</h2>
+    <el-menu :router="true" :default-active="activeIndex2" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <el-menu-item index="/">处理中心</el-menu-item>
+      <el-menu-item index="/about">订单管理</el-menu-item>
+    </el-menu>
+    <div>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1'
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<style></style>
